80daa20b3bcb39f93a13aa6383c23011df84a55b
PauStasz <paulinastasz02@gmail.com>
2024-04-18 18:16:24 +0200

    struktura pod wzorzec mvvm

---
+++ Database/FirebaseRealTime/Firebase.cs
@@ -0,0 +1,34 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace Wallet.Database.FirebaseRealTime
+{
+    internal class Firebase
+    {
+        private static Firebase _instance;
+        private IFirebaseConfig _config;
+
+        private Firebase()
+        {
+            _config = new FirebaseConfig
+            {
+                AuthSecret = "S8FGNt0XY2j26r2vC0yA2PAxCRUSTKUTOScxyszs",
+                BasePath = "https://portfel-a3197-default-rtdb.firebaseio.com/",
+            };
+        }
+
+        public static Firebase Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Firebase();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
---
+++ Models/users/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet.Models.users
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Nick {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
