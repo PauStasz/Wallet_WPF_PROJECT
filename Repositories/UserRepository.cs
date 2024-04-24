0c809c8aa774668c38245de4a61854851aeb4d9a
Isia07 <aga.p.10@wp.pl>
2024-04-24 04:39:46 +0200

    generate id, hash password, verify password

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,5 +1,7 @@
-﻿using Wallet.Models.Users;
+﻿using System.Text;
+using Wallet.Models.Users;
 using Wallet.Repositories.IRepositories;
+using System.Security.Cryptography;
 
 namespace Wallet.Repositories
 {
@@ -8,12 +10,48 @@ namespace Wallet.Repositories
         public UserRepository() : base()
         {
 
+        }        
+
+        private int GenerateUserID()
+        {
+            Guid newGuid = Guid.NewGuid();
+            byte[] newGuidByte = newGuid.ToByteArray();
+            int intId = BitConverter.ToInt32(newGuidByte, 0);
+
+            return intId;
+        }
+
+        public static string HashPassword(string password)
+        {
+            using var newSha256 = SHA256.Create();
+            {
+                byte[] shaBytes = newSha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var sb = new StringBuilder();
+
+                foreach (byte b in shaBytes)
+                {
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public static bool VerifyPassword(string password, string hashedPasswordFromDB)
+        {
+            string passwordToVerify = HashPassword(password);
+            
+            if (passwordToVerify == hashedPasswordFromDB)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public override void SetData(string nameTable, User entity)
         {
-            //enity.Id = przdzielanie id lub w klasie BaseObject 
-            //entity.HashPassword = hasowanie hasła
+            entity.Id = GenerateUserID();
+            entity.HashPassword = HashPassword(entity.Password);
             base.SetData(nameTable, entity);    
         }
 
