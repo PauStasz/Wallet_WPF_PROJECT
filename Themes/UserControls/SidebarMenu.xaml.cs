71c1d9d6f13161a4b92119ec0d686516e58b295b
PauStasz <paulinastasz02@gmail.com>
2024-05-07 18:33:43 +0200

    okna: konto i kategorie

--- Themes/UserControls/SidebarMenu.xaml.cs
+++ Themes/UserControls/SidebarMenu.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wallet.Views;
 
 namespace Wallet.Themes.UserControls
 {
@@ -23,6 +24,86 @@ namespace Wallet.Themes.UserControls
         public SidebarMenu()
         {
             InitializeComponent();
+        }
+
+        private void GoHome(object sender, RoutedEventArgs e)
+        {
+           
+            Window currentWindow = Application.Current.MainWindow;
+
+            if (currentWindow is not null)
+            {
+                HomeWindow homeWindow = new HomeWindow();
+                currentWindow.Content = homeWindow.Content;
+            }
+        }
+
+        private void GoExpensePlanning(object sender, RoutedEventArgs e)
+        {
+            Window currentWindow = Application.Current.MainWindow;
+
+            if (currentWindow is not null)
+            {
+                ExpensePlanningWindow window = new ExpensePlanningWindow();
+                currentWindow.Content = window.Content;
+            }
+        }
+
+        private void GoRaport(object sender, RoutedEventArgs e)
+        {
+            Window currentWindow = Application.Current.MainWindow;
+
+            if (currentWindow is not null)
+            {
+                ReportAndStatisticsWindow window = new ReportAndStatisticsWindow();
+                currentWindow.Content = window.Content;
+            }
+        }
+
+        private void GoForecast(object sender, RoutedEventArgs e)
+        {
+            Window currentWindow = Application.Current.MainWindow;
+
+            if (currentWindow is not null)
+            {
+                ForecastWindow window = new ForecastWindow();
+                currentWindow.Content = window.Content;
+ 
+            }
+        }
+
+        private void GoAccount(object sender, RoutedEventArgs e)
+        {
+            Window currentWindow = Application.Current.MainWindow;
+
+            if (currentWindow is not null)
+            {
+                AccountsWindow window = new AccountsWindow();
+                currentWindow.Content = window.Content;
+
+            }
+        }
+
+        private void GoCategories(object sender, RoutedEventArgs e)
+        {
+            Window currentWindow = Application.Current.MainWindow;
+
+            if (currentWindow is not null)
+            {
+                CategoriesWindow window = new CategoriesWindow();
+                currentWindow.Content = window.Content;
+            }
+        }
+
+        private void GoSettings(object sender, RoutedEventArgs e)
+        {
+            Window currentWindow = Application.Current.MainWindow;
+
+            if (currentWindow is not null)
+            {   
+                SettingsWindow window = new SettingsWindow();
+                currentWindow.Content = window.Content;
+            }
         }
     }
 }
